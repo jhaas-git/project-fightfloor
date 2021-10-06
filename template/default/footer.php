0dbaafb0c79b0b539b4cc9b3810648e601227514
JH-Development <joeyhaas.git@gmail.com>
2021-10-06 18:26:42 +0200

    Responsive footer
    Simple responsive footer which contains useful links and a newsletter form.

---
+++ template/default/footer.php
@@ -0,0 +1,40 @@
+<footer>
+    <div class="footer-container">
+        <div class="footer-row">
+            <div class="column">
+                <div class="column-content">
+                    <h3>Service en Informatie</h3>
+                    <ul>
+                        <li><a href="#">Klantenservice</a></li>
+                        <li><a href="#">Huisregels</a></li>
+                        <li><a href="#">Openingstijden</a></li>
+                        <li><a href="#">Contact Opnemen</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column">
+                <div class="column-content">
+                    <div class="content-right">
+                        <h3>Ons bedrijf</h3>
+                        <ul>
+                            <li><a href="#">Fight Floor</a></li>
+                            <li><a href="#">Onze Lessen</a></li>
+                            <li><a href="#">Rooster bekijken</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="column" id="newsletter-placement">
+                <div class="column-content">
+                    <h3>Nieuwsbrief</h3>
+                    <p>Blijf op de hoogte van aanbiedingen en toekomstige lessen!</p>
+                    <form action="">
+                        <input type="text" placeholder="e-mailadres">
+                        <br>
+                        <button>inschrijven</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- template/home.php
+++ template/home.php
@@ -5,11 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../design/default/header.css">
+    <link rel="stylesheet" href="../design/pages/home.css">
+    <link rel="stylesheet" href="../design/default/footer.css">
     <title>Home | Fight Floor</title>
 </head>
 <body>
     <?php include 'default/header.php' ?>
 
+    <main>
+
+    </main>
+
+    <?php include 'default/footer.php' ?>
 
     <script src="../javascript/script.js"></script>
 </body>
