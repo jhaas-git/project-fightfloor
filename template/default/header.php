b1b6792c12c346eb7e14e5220ef70b2ae8b102f4
JH-Development <joeyhaas.git@gmail.com>
2021-10-06 18:15:55 +0200

    Responsive header
    Responsive fixed header that consists of dynamic links that change when a user is logged in or not.
    
    On smaller viewports links are replaced with an animated burger menu. This works with CSS and Javascript.
    
    Javascript adds an overlay class, which will be made later on in the project. Defending on the content that will be needed.

---
+++ template/default/header.php
@@ -0,0 +1,32 @@
+<header>
+    <div class="header-container">
+        <div class="header-left">
+            <h3>Fight Floor</h3>
+        </div>
+        <!-- Animated burger-menu only displayed when viewport-
+             is less than 600px wide.  -->
+        <div class="header-mobile" id="burger-menu">
+            <span></span>
+        </div>
+        <div class="header-right">
+            <ul class="desktop">
+                <li><a href="planning.php">rooster</a></li>
+                <li><a href="#">huisregels</a></li>
+                <?php 
+                // Only display the link to admin panel when the logged in user has the
+                // correct roles. 1 = eigenaar, 2 = coach.
+                if ($_SESSION['rollen_idRol'] == 1 || $_SESSION['rollen_idRol'] == 2) {
+                    echo "<li><a href='adminpanel.php'>admin paneel</a></li>";
+                }
+                // Display the 'aanmelden' button only when no session is set.
+                // Session running? It will link to the profile with a greeting.
+                if(!isset($_SESSION['loggedin'])) {
+                    echo "<li><a href='../template/aanmelden.php' class='loginBtn'>inloggen</a></li>";
+                } else {
+                    echo "<li><a href='../template/profile.php'>Hallo, <span>". $_SESSION["sVoornaam"] ."</span></a></li>";
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+</header>
